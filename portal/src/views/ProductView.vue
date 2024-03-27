c927211a1199a3d8eabfe0fd361e28668c69cf2a
Lsx <1778416955@qq.com>
2024-03-27 22:32:45 +0800

    slider

--- portal/src/views/ProductView.vue
+++ portal/src/views/ProductView.vue
@@ -1,12 +1,29 @@
 <template>
 	<Layout>
 		<div class="layout-container" style="width: 100%">
-				<!-- <div class="carousel">
+			<!-- <div class="carousel">
 					<div class="slide" v-for="(image, index) in images" :key="index" :style="{ backgroundImage: 'url(' + image + ')' }">
 					</div>
 				</div> -->
-				<div class="solution-page">
-
+			<!-- <div class="solution-page">
+				<div class="carousel">
+					<div class="carousel-inner" ref="carouselInner">
+						<div class="carousel-item" v-for="(item, index) in slideshow" :key="index">
+							<img :src="item.imageUrl" :alt="item.alt" >
+						</div>
+					</div>
+					<button @click="prev">prev</button>
+					<button @click="next">next</button>
+				</div>
+			</div> -->
+			<div class="carousel">
+					<div class="carousel-inner" ref="carouselInner">
+						<div class="carousel-item" v-for="(item, index) in slideshow" :key="index">
+							<img :src="item.imageUrl" :alt="item.alt" >
+						</div>
+					</div>
+					<!-- <button @click="prev">prev</button>
+					<button @click="next">next</button> -->
 				</div>
 		</div>
 		<div class="layout-container" style="width: 100%">
@@ -19,7 +36,7 @@
 				</div>
 			</div>
 		</div>
-		
+
 		<div class="section">
 			<div class="container" style="max-width: 1160px">
 				<div class="section--header">
@@ -28,25 +45,21 @@
 						We have a wide range of products covering a wide range of businesses
 					</p>
 				</div>
-				
+
 				<div class="solution-container">
 					<div data-am-widget="tabs" class="am-tabs ">
 						<ul class="am-tabs-nav am-cf">
-							<li v-for="(tab,index) in tabList"
-								:key="index"
-								@click.prevent="changeTab(index,tab.typeId)"
-								:class="tabIndex === index ? 'am-active':''">
-								<a href="#">{{tab.typeName}}</a>
+							<li v-for="(tab, index) in tabList" :key="index" @click.prevent="changeTab(index, tab.typeId)"
+								:class="tabIndex === index ? 'am-active' : ''">
+								<a href="#">{{ tab.typeName }}</a>
 							</li>
 						</ul>
 						<div class="am-tabs-bd">
 							<div class=am-tabs-tab>
-								<div class="am-tab"
-									v-for="(goods,index) in goodsList"
-									:key="index"
+								<div class="am-tab" v-for="(goods, index) in goodsList" :key="index"
 									@click="handleDetails(goods.typeDetaisId)">
 									<img :src="goods.imageUrl1" alt="">
-									<p>{{goods.title}}</p>
+									<p>{{ goods.title }}</p>
 								</div>
 							</div>
 						</div>
@@ -54,7 +67,7 @@
 				</div>
 			</div>
 		</div>
-		
+
 	</Layout>
 </template>
 
@@ -62,47 +75,115 @@
 import Layout from "@/components/common/Layout";
 export default {
 	name: "ProductView",
-	components: {Layout},
-	data(){
-		return{
-			tabList:[],
-			list:[],
+	components: { Layout },
+	data() {
+		return {
+			tabList: [],
+			list: [],
 			tabIndex: 0,
-			goodsList:[]
+			goodsList: [],
+			currentIndex: 0,
+			intervalId: null,
+			// items: [
+			// 	{ image: '../assets/images/about/ben.png', alt: 'Image 1'},
+			// 	{ image: '../assets/images/about/ben.png', alt: 'Image 2'},
+			// 	{ image: '../assets/images/about/ben.png', alt: 'Image 3'}
+			// ]
+			slideshow:[],
 		}
 	},
 	mounted() {
 		this.getTabList()
 		this.getGoodsList(1)
+		this.startAutoplay()
+		this.getSlideshow()
+		this.$refs.carouselInner.addEventListener('mouseover', this.stopAutoplay)
+		this.$refs.carouselInner.addEventListener('mouseout', this.startAutoplay);
 	},
-	methods:{
-		getTabList(){
-			this.getRequest("/findAllType").then(resp =>{
-				if (resp){
+	methods: {
+		getTabList() {
+			this.getRequest("/findAllType").then(resp => {
+				if (resp) {
 					this.tabList = resp.data.data
 					//console.log(resp.data.data)
 				}
 			})
 		},
-		changeTab(index,typeId){
+		changeTab(index, typeId) {
 			this.tabIndex = index
 			console.log(typeId)
 			this.getGoodsList(typeId)
 		},
-		getGoodsList(typeId){
-			this.getRequest(`/findTypeDetailsByTypeId/${typeId}`).then(resp =>{
-				if (resp){
+		getGoodsList(typeId) {
+			this.getRequest(`/findTypeDetailsByTypeId/${typeId}`).then(resp => {
+				if (resp) {
 					this.goodsList = resp.data.data
 					//console.log(resp.data.data)
 				}
 			})
+		},
+		getSlideshow() {
+			this.getRequest("/findAllSlideshow").then(resp => {
+				if (resp) {
+					this.slideshow = resp.data.data
+					// console.log(this.slideshow)
+				}
+			})
+		},
+		next() {
+			this.currentIndex = (this.currentIndex + 1) % this.slideshow.length;
+			this.updateCarousel();
+		},
+		prev() {
+			this.currentIndex = (this.currentIndex - 1 + this.slideshow.length) % this.slideshow.length;
+			this.updateCarousel();
+		},
+		updateCarousel() {
+			this.$refs.carouselInner.style.transform = `translateX(-${this.currentIndex * 100}%)`;
+		},
+		startAutoplay() {
+			this.intervalId = setInterval(this.next, 3000); // 每3秒切换到下一个
+		},
+		stopAutoplay() {
+			clearInterval(this.intervalId);
 		}
+	},
+	beforeDestroy() {
+		this.stopAutoplay();
+		this.$refs.carouselInner.removeEventListener('mouseover', this.stopAutoplay);
+		this.$refs.carouselInner.removeEventListener('mouseout', this.startAutoplay);
 	}
 }
 </script>
 
 <style scoped>
+.carousel {
+	position: relative;
+	width: 100%;
+	overflow: hidden;
+}
 
+.carousel-inner {
+	display: flex;
+	transition: transform 1s ease;
+}
 
+.carousel-item {
+	flex: 0 0 100%;
+	text-align: center;
+}
+
+.carousel-item img {
+	width: 100%;
+	height: 300px;
+	display: block;
+	margin: 0 auto;
+}
+
+button {
+	cursor: pointer;
+	padding: 10px 20px;
+	margin-top: 10px;
+}
 
 </style>
